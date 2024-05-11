2821bb1cec1eb0ea52aa770e132c23df2db4f207
TimWong <timwong.tw93@gmail.com>
2024-05-11 18:13:52 +1000

    added a lib folder to hold svg code

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const inquirer = require(`inquirer`);
+// const document = require(`./lib/document`);
+const {Shapes} = require(`./lib/shapes`);
+const {Logo} = require(`./lib/text`);
+// const fs = require(`fs`);
 
 inquirer
   .prompt ([
@@ -17,7 +21,7 @@ inquirer
     {
       type: `list`,
       message: `Choose which shape the logo will be:`,
-      choices: [`Circle`, `Square`, `Triangle`, `Star`, `Diamond`],
+      choices: [`Circle`, `Eclispe`,`Square`, `Rectangle`, `Star`],
       name: `logoShape`,
     },
 
@@ -28,9 +32,15 @@ inquirer
     },
   ])
   .then((response) => {
-    console.log(`This is what you chose:`);
-    console.log(response.logoText);
-    console.log(response.logoTextColour);
-    console.log(response.logoShape);
-    console.log(response.logoShapeColour);
+    // console.log(`This is what you chose:`);
+    // console.log(response.logoText);
+    // console.log(response.logoTextColour);
+    // console.log(typeof(response.logoShape));
+    // console.log(response.logoShapeColour);
+
+    const logo = Shapes(response.logoShape, response.logoShapeColour);
+    console.log(logo);
+
+    const text = Logo(response.logoText, response.logoTextColour);
+    console.log(text);
   });
